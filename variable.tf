f618a04af119302963a92e1d9ab5ddc2c0fa48bd
neha <nbahuguna884@gmail.com>
2024-03-31 13:32:47 +0530

    add  file

---
+++ variable.tf
@@ -0,0 +1,16 @@
+variable "name" {
+  description = "(Required) The name of the resource group. Must be unique on your Azure subscription"
+  default     = ""
+}
+
+
+variable "location" {
+  description = "(Required) The name of the resource group. Must be unique on your Azure subscription"
+  default     = ""
+}
+
+
+variable "tags" {
+  description = "(Required) The name of the resource group. Must be unique on your Azure subscription"
+  default     = ""
+}
